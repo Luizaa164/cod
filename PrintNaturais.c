6ef8285d40f1e57b7eda66f45585810a0cf67de6
Luizaa164 <56280739+Luizaa164@users.noreply.github.com>
2019-10-07 20:33:10 -0300

    Create PrintNaturais.c

---
+++ PrintNaturais.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num = 0;
+    while (num < 100){
+        printf ("%d\n", num);
+        num++;
+    }
+	return 0;
+}
